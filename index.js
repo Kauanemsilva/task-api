3d99f3bd14b22bedab3202a6f143eed01a5a97e6
Kauane Silva <seuemail@example.com>
2025-07-16 16:41:03 -0300

    Versão rota raiz

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('🚀 API de Tarefas da Kauane rodando com sucesso!');
+});
